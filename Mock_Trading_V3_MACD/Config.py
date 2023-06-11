0929f20b1a7e123afb66d3d74165bd5810fa0511
Ray Ching <71825980+LILRAY0826@users.noreply.github.com>
2023-06-11 23:21:52 +0800

    2023-06-11 - 23:21

--- Mock_Trading_V3_MACD/Config.py
+++ Mock_Trading_V3_MACD/Config.py
@@ -1,11 +1,8 @@
 from binance.client import Client
 from binance.um_futures import UMFutures
-from datetime import datetime
 from tqdm import tqdm
 import pandas as pd
-import math
-import time
-import os
+
 pd.set_option('mode.chained_assignment', None)
 
 
@@ -21,20 +18,29 @@ class Functions:
 
         long_position = [[None, None, None, None, None, None]]
         short_position = [[None, None, None, None, None, None]]
+        long_result = None
+        short_result = None
+        trading_number = 0
+        win_number = 0
+        uncertain_number = 0
         position_history = pd.DataFrame(columns=["klines_open_time", "klines_open_price", "klines_high_price", "klines_low_price", "klines_close_price", "space 1",
                                                  "order_entry_time", "order_entry_direction", "order_price", "order_quantity", "space 2",
                                                  "long_position_entry_time", "long_position_entry_direction", "long_position_price", "long_position_quantity", "long_position_afford_range", "long_position_liquidation", "space 3",
                                                  "short_position_entry_time", "short_position_entry_direction", "short_position_price", "short_position_quantity", "short_position_afford_range", "short_position_liquidation", "space 4",
-                                                 "Result", "Property"])
+                                                 "LONG_Result", "SHORT_Result", "Property"])
 
         # Iterate klines to simulate the market
         progress = tqdm(total=len(klines_dataframe))
         for index, row in klines_dataframe.iterrows():
 
             progress.update(1)
-            result = None
+            if long_result != None:
+                long_position = [[None, None, None, None, None, None]]
 
-            open_time = str(row['Open Time'])[:10]
+            if short_result != None:
+                short_position = [[None, None, None, None, None, None]]
+
+            open_time = str(row['Open Time'])
             open_price = row['open']
             high_price = row['high']
             low_price = row['low']
@@ -46,104 +52,126 @@ class Functions:
             else:
                 quantity = round((buy * leverage) / entry_price, 2)
 
-            # Observe Exit Signal
-            if long_position[0][0] != None:
-                if low_price < long_position[0][4] < high_price:
-                    start_property += long_position[0][3] * (long_position[0][4] - long_position[0][2])
-                    long_position = [[None, None, None, None, None, None]]
-                    result = "Win"
-                elif long_position[0][5] > low_price:
-                    start_property += long_position[0][3] * (long_position[0][5] - long_position[0][2])
-                    long_position = [[None, None, None, None, None, None]]
-                    result = "Lose"
-                elif long_position[0][5] < low_price and high_price > long_position[0][4]:
-                    long_position = [[None, None, None, None, None, None]]
-                    result = "Uncertainly"
-                else:
-                    result = None
-
-            if short_position[0][0] != None:
-                if low_price < short_position[0][4] < high_price:
-                    start_property += short_position[0][3] * (short_position[0][2] - short_position[0][4])
-                    short_position = [[None, None, None, None, None, None]]
-                    result = "Win"
-                elif short_position[0][5] < high_price:
-                    start_property += short_position[0][3] * (short_position[0][2] - short_position[0][5])
-                    short_position = [[None, None, None, None, None, None]]
-                    result = "Lose"
-                elif short_position[0][5] < high_price and low_price < short_position[0][4]:
-                    short_position = [[None, None, None, None, None, None]]
-                    result = "Uncertainly"
-                else:
-                    result = None
-
             # Create Position
             if boolean and start_property > buy:
                 # Long Position
                 if "多" in entry_direction:
 
                     if long_position[0][0] == None:
+                        trading_number += 1
                         liquidation = round(entry_price * (1 - (100 / leverage) / 100), 2)
                         long_position = [[entry_time, entry_direction, entry_price, quantity, entry_price + afford_range, liquidation]]
                     else:
-                        long_position[0][0] = entry_time
-                        times = long_position[0][3] / quantity
-                        long_position[0][2] = (long_position[0][2] * times + entry_price) / (times + 1)
-                        long_position[0][3] += quantity
-                        long_position[0][4] = long_position[0][2] + afford_range
-                        long_position[0][5] = round(long_position[0][2] * (1 + (100 / leverage) / 100), 2)
+                        if long_position[0][2] - anti_direction_choppy < long_position[0][2] < long_position[0][2] + direction_choppy:
+                            entry_time, entry_price, entry_direction, quantity = None, None, None, None
+                            pass
+                        else:
+                            trading_number += 1
+                            long_position[0][0] = entry_time
+                            times = long_position[0][3] / quantity
+                            long_position[0][2] = (long_position[0][2] * times + entry_price) / (times + 1)
+                            long_position[0][3] += quantity
+                            long_position[0][4] = long_position[0][2] + afford_range
+                            long_position[0][5] = round(long_position[0][2] * (1 - (100 / leverage) / 100), 2)
 
                 # Short Position
                 elif "空" in entry_direction:
 
                     if short_position[0][0] == None:
+                        trading_number += 1
                         liquidation = round(entry_price * (1 + (100 / leverage) / 100), 2)
                         short_position = [[entry_time, entry_direction, entry_price, quantity, entry_price - afford_range, liquidation]]
                     else:
-                        short_position[0][0] = entry_time
-                        times = short_position[0][3] / quantity
-                        short_position[0][2] = (short_position[0][2] * times + entry_price) / (times + 1)
-                        short_position[0][3] += quantity
-                        short_position[0][4] = short_position[0][2] - afford_range
-                        short_position[0][5] = round(short_position[0][2] * (1 - (100 / leverage) / 100), 2)
+                        if short_position[0][2] - direction_choppy < short_position[0][2] < short_position[0][2] + anti_direction_choppy:
+                            entry_time, entry_price, entry_direction, quantity = None, None, None, None
+                            pass
+                        else:
+                            trading_number += 1
+                            short_position[0][0] = entry_time
+                            times = short_position[0][3] / quantity
+                            short_position[0][2] = (short_position[0][2] * times + entry_price) / (times + 1)
+                            short_position[0][3] += quantity
+                            short_position[0][4] = short_position[0][2] - afford_range
+                            short_position[0][5] = round(short_position[0][2] * (1 + (100 / leverage) / 100), 2)
+
+            # Observe Exit Signal
+            if long_position[0][0] != None:
+                if low_price < long_position[0][4] < high_price and long_position[0][5] < low_price:
+                    start_property += long_position[0][3] * (long_position[0][4] - long_position[0][2])
+                    long_result = "LONG Win"
+                    win_number += 1
+                elif low_price < long_position[0][4] < high_price and long_position[0][5] > low_price:
+                    long_result = "Uncertain"
+                    uncertain_number += 1
+                elif long_position[0][5] > low_price:
+                    start_property += long_position[0][3] * (long_position[0][5] - long_position[0][2])
+                    long_result = "LONG Lose"
+                else:
+                    long_result = None
+            else:
+                long_result = None
+
+            if short_position[0][0] != None:
+                if low_price < short_position[0][4] < high_price and short_position[0][5] > high_price:
+                    start_property += short_position[0][3] * (short_position[0][2] - short_position[0][4])
+                    short_result = "SHORT Win"
+                    win_number += 1
+                elif low_price < short_position[0][4] < high_price and short_position[0][5] < high_price:
+                    short_result = "Uncertain"
+                    uncertain_number += 1
+                elif short_position[0][5] < high_price:
+                    start_property += short_position[0][3] * (short_position[0][2] - short_position[0][5])
+                    short_result = "SHORT Lose"
+                else:
+                    short_result = None
+            else:
+                short_result = None
 
             # Record Position Situation
             list = [[open_time, open_price, high_price, low_price, close_price, None,
                     entry_time, entry_direction, entry_price, quantity, None,
                     long_position[0][0], long_position[0][1], long_position[0][2], long_position[0][3], long_position[0][4], long_position[0][5], None,
                     short_position[0][0], short_position[0][1], short_position[0][2], short_position[0][3], short_position[0][4], short_position[0][5],
-                    result, start_property]]
+                    long_result, short_result, start_property]]
 
             if index == 0:
                 position_history = pd.DataFrame(list, columns=["klines_open_time", "klines_open_price", "klines_high_price", "klines_low_price", "klines_close_price", "space 1",
                                                                "order_entry_time", "order_entry_direction", "order_price", "order_quantity", "space 2",
                                                                "long_position_entry_time", "long_position_entry_direction", "long_position_price", "long_position_quantity", "long_position_afford_range", "long_position_liquidation", "space 3",
                                                                "short_position_entry_time", "short_position_entry_direction", "short_position_price", "short_position_quantity", "short_position_afford_range", "short_position_liquidation",
-                                                               "Result", "Property"])
+                                                               "LONG_Result", "SHORT_Result", "Property"])
             else:
                 temp_dataframe = pd.DataFrame(list, columns=["klines_open_time", "klines_open_price", "klines_high_price", "klines_low_price", "klines_close_price", "space 1",
                                                              "order_entry_time", "order_entry_direction", "order_price", "order_quantity", "space 2",
                                                              "long_position_entry_time", "long_position_entry_direction", "long_position_price", "long_position_quantity", "long_position_afford_range", "long_position_liquidation", "space 3",
                                                              "short_position_entry_time", "short_position_entry_direction", "short_position_price", "short_position_quantity", "short_position_afford_range", "short_position_liquidation",
-                                                             "Result", "Property"])
+                                                             "LONG_Result", "SHORT_Result", "Property"])
                 position_history = pd.concat([position_history, temp_dataframe])
 
         position_history.to_csv("Position History.csv", encoding='utf_8_sig')
+        print("Uncertain = {}".format(uncertain_number))
+        return list[0][-1], round(win_number/trading_number, 2)*100
 
     # About Market's information
     # Get 300 pass klines data
-    def get_klines(self, start_date, end_date):
+    def get_klines(self, start_date, end_date, interval):
 
         # Get klines dataframe
+        if interval == "1hr":
+            interval = Client.KLINE_INTERVAL_1HOUR
+        elif interval == "4hr":
+            interval = Client.KLINE_INTERVAL_4HOUR
+        elif interval == "1day":
+            interval = Client.KLINE_INTERVAL_1DAY
         dataframe = pd.DataFrame(self.client.get_historical_klines(symbol="BTCUSDT",
-                                                                   interval=Client.KLINE_INTERVAL_1DAY,
+                                                                   interval=interval,
                                                                    start_str=start_date,
                                                                    end_str=end_date)).drop(columns=[5, 7, 8, 9, 10, 11])
         dataframe.columns = ['Open Time', 'open', 'high', 'low', 'close', 'Close Time']
 
         # Convert timestamp to datetime
-        dataframe['Open Time'] = pd.to_datetime((dataframe['Open Time'] + 288e5) / 1000, unit='s')
-        dataframe['Close Time'] = pd.to_datetime((dataframe['Close Time'] + 288e5) / 1000, unit='s')
+        dataframe['Open Time'] = pd.to_datetime((dataframe['Open Time']) / 1000, unit='s')
+        dataframe['Close Time'] = pd.to_datetime((dataframe['Close Time']) / 1000, unit='s')  # 288e5
 
         # Change Data Type in Dataframe
         for column in dataframe.columns:
@@ -163,8 +191,8 @@ class Functions:
             # Get Entry Time
             if "進場" in kind:
                 for index_, open_time in enumerate(klines_dataframe["Open Time"]):
-                    if str(mock_trading_dataframe["日期/時間"][index])[:10] == str(open_time)[:10]:
-                        entry_data.append([str(open_time)[:10], float(klines_dataframe['open'][index_]), kind, index_])
+                    if str(mock_trading_dataframe["日期/時間"][index]) == str(open_time):
+                        entry_data.append([str(open_time), float(klines_dataframe['open'][index_]), kind, index_])
                         break
                     else:
                         pass
@@ -175,19 +203,13 @@ class Functions:
         return entry_data
 
     def open_time_is_entry_time(self, open_time: str, entry_data):
-        open_time = int(open_time.replace("-", ""))
 
         for index, data in enumerate(entry_data):
             entry_time = data[0]
             entry_price = data[1]
             entry_direction = data[2]
-            entry_time_int = int(entry_time.replace("-", ""))
 
-            if open_time == entry_time_int:
+            if open_time == entry_time:
                 return True, entry_time, entry_price, entry_direction
 
-            elif open_time < entry_time_int:
-                return False, None, None, None
-
-            elif index == len(entry_data)-1 and open_time != entry_time_int:
-                return False, None, None, None
+        return False, None, None, None
--- Mock_Trading_V3_MACD/Main.py
+++ Mock_Trading_V3_MACD/Main.py
@@ -6,26 +6,28 @@ api_key = 'Nsw6xqsnHbfLyF8YExEfeVVJz4Jn3F4c4VHCr5L5sl8n8KUKzsPazLx4IX4dMaPD'
 api_secret = '5ggLxtySNfzAE64bRgXz6IYHxDtaIDx399FTG1FE9IBd4332AxNBAazeKOCW9HHv'
 
 # Klines Parameter
-start_date = "1 Jan, 2022"
-end_date = "1 Jan, 2023"
+start_date = "1 Jan, 2020"
+end_date = "1 Jan, 2021"
+interval = "1day"  # 1hr, 4hr, 1day
+csv_path = "Trading_View_1Day.csv"
 
 # Mock Trading Parameter
-initial_property = 10000
-buy = 2000
-leverage = 10
-afford_range = 200
-direction_choppy = 250
-anti_direction_choppy = 800
+initial_property = 8000
+buy = 800
+leverage = 25
+afford_range = 1000
+direction_choppy = 0  # The critical value of direction of position to avoid choppy trend
+anti_direction_choppy = 0  # The critical value of anti direction of position to avoid choppy trend
 
 if __name__ == '__main__':
     print("System Initializing...")
     functions = Functions(api_key, api_secret)
 
     print("Digging Klines Data...")
-    functions.get_klines(start_date, end_date)
+    functions.get_klines(start_date, end_date, interval)
 
     print("Reading Mock Trading From Trading View...")
-    mock_trading_dataframe = pd.read_excel("Mock Trading List.xlsx")
+    mock_trading_dataframe = pd.read_csv(csv_path)
     mock_trading_dataframe['日期/時間'] = pd.to_datetime(mock_trading_dataframe['日期/時間'], format="%Y/%m/%d")
 
     print("Reading Klines.csv...")
@@ -41,16 +43,14 @@ if __name__ == '__main__':
     entry_data = functions.get_entry_time_price(klines_dataframe, mock_trading_dataframe)
 
     print("Stimulate Mock Trading...")
-    functions.mock_trading(entry_data=entry_data,
-                           klines_dataframe=klines_dataframe,
-                           start_property=initial_property,
-                           buy=buy,
-                           leverage=leverage,
-                           afford_range=afford_range,
-                           direction_choppy=direction_choppy,
-                           anti_direction_choppy=anti_direction_choppy)
+    initial_property, winning_percentage = functions.mock_trading(entry_data=entry_data,
+                                                                  klines_dataframe=klines_dataframe,
+                                                                  start_property=initial_property,
+                                                                  buy=buy,
+                                                                  leverage=leverage,
+                                                                  afford_range=afford_range,
+                                                                  direction_choppy=direction_choppy,
+                                                                  anti_direction_choppy=anti_direction_choppy)
+    print("Winning Percentage = {}%\nProperty = {}".format(winning_percentage, initial_property))
+    print("Leverage : {}, Afford Range : {}".format(leverage, afford_range))
     print("Done!")
-
-
-
-
