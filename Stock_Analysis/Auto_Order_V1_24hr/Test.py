d95dc6495c6000cdfdc4f7efcb026f802f5dc919
Ray Ching <71825980+LILRAY0826@users.noreply.github.com>
2023-06-29 16:38:14 +0800

    202306291638

--- Stock_Analysis/Auto_Order_V1_24hr/Test.py
+++ Stock_Analysis/Auto_Order_V1_24hr/Test.py
@@ -1,13 +1,3 @@
-from binance.client import Client
-from binance.um_futures import UMFutures
-import pandas as pd
-import talib
-import math
-
-api_key = 'dGpm78xWKGPzgbTZntqrfaAGGWzxam7awweyHImxyTTWxa4HPPGnKZTRZbz34TNV'
-api_secret = 'jTIsMOCMvGrRCmfVSLsITdcOErR7xg3IuexviGtAfdeeOiojiswEPIFS5iuRGDz5'
-
-client = Client(api_key=api_key,
-                api_secret=api_secret)
-while True:
-    print(client.futures_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_4HOUR, limit=1)[0][4])
+a = []
+for item in a:
+    print(item)
